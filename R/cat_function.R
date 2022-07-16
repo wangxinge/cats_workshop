8a0a2f0200f651b16ec727f16cd7db16290ab79c
Xinge Wang <wang.xinge@outlook.com>
2022-07-16 18:40:02 -0500

    update

--- R/cat_function.R
+++ R/cat_function.R
@@ -2,10 +2,7 @@
 #'
 #' This function allows you to express your love of cats.
 #' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
 #' @export
-#' @examples
-#' cat_function()
 cat_function <- function(love=TRUE){
   if(love==TRUE){
     print("I love cats!")
